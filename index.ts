a3a1651d4ad9fb9f64fa483f8b08882933f145b0
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-01-03 16:28:14 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { Connection } from "./structures/Connection";
+import { Filters } from "./structures/Filters";
+import { Node } from "./structures/Node";
+import { Aqua } from "./structures/Aqua";
+import { Player } from "./structures/Player";
+import { Plugin } from "./structures/Plugins";
+import { Queue } from "./structures/Queue";
+import { Rest } from "./structures/Rest";
+import { Track } from "./structures/Track";
+
+export { Connection, Filters, Node, Aqua, Player, Plugin, Queue, Rest, Track };
